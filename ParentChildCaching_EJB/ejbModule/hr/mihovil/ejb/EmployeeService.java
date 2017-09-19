18d96cb37fe0808771fca462d5cd1082a697452c
Mihovil Frater <mihovil.frater@neos.hr>
2017-09-19 20:57:03 +0200

    making it more effecient

--- ParentChildCaching_EJB/ejbModule/hr/mihovil/ejb/EmployeeService.java
+++ ParentChildCaching_EJB/ejbModule/hr/mihovil/ejb/EmployeeService.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import hr.mih0vil.parentchild.Department;
 import hr.mih0vil.parentchild.Employee;
+import hr.mih0vil.parentchild.EmployeeWithDeparment;
 
 /**
  * Session Bean implementation class EmployeeService
@@ -33,5 +36,23 @@ public class EmployeeService {
     	return em.createNamedQuery("Department.Employee.getHigherPayed", 
     			Department.class).getResultList();
     }
+    
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)   
+    public List<Employee> getEmployeesHigherPayedWithDeparments() {
+    	List<Employee> emps = getEmployeesHigherPayed(); //get employees
+    	List<Department> deps = getDepartmentsForEmployeesHigherPayed(); //get departments to which retrieved employees relate to
+    	for (Employee e: emps) {
+    		e.getDepartment(); //get the department from cache now while we are in the transaction
+    	}
+    	return emps;
+    }
+    
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)   
+    public List<EmployeeWithDeparment> getHigherPayedEmpoyeesWithDepartment() {
+    	return em.createNamedQuery("EmployeeWithDeparment.getHigherPayed", 
+    			EmployeeWithDeparment.class).getResultList();
+    }
+    
+    
     
 }
